ff7a8d712ca6183ee605094677528933016593e5
xgibertt <xgibert@gmail.com>
2017-09-29 20:45:54 +0200

    New models (Game & Round)

--- mastermind/game/models.py
+++ mastermind/game/models.py
@@ -1,3 +1,23 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+COLORS = (
+    ('R', 'RED'),
+    ('B', 'BLUE'),
+    ('G', 'GREEN'),
+    ('Y', 'YELLOW'),
+)
+
+
+class Game(models.Model):
+    code = ArrayField(models.CharField(max_length=1, choices=COLORS), max_length=4)
+    ended = models.BooleanField(default=False)
+    won = models.BooleanField(default=False)
+
+
+class Round(models.Model):
+    group = models.ForeignKey(Game, on_delete=models.CASCADE)
+    code = ArrayField(models.CharField(max_length=1, choices=COLORS), max_length=4)
+    black_pegs = models.IntegerField(null=True)
+    white_pegs = models.IntegerField(null=True)
